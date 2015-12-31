cc4f5dbaaf5a91d16f79ba9be52e07ec60f18cdb
Brandon Kase <brandon.kase@gmail.com>
2015-12-31 01:49:59 -0500

    Implement debug for SHA1Hash{es}

--- src/sha1bytes/mod.rs
+++ src/sha1bytes/mod.rs
@@ -1,13 +1,20 @@
 use std::iter;
 use std::mem;
-use std::borrow::{Cow, ToOwned};
+use std::fmt;
+use std::borrow::{Cow, ToOwned, Borrow};
 use std::iter::{Iterator, IntoIterator, ExactSizeIterator};
 use std::slice::Chunks;
 use crypto::digest::Digest;
 use crypto::sha1::Sha1;
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 pub struct SHA1Hash<'a>(Cow<'a, [u8]>);
+
+impl <'a> fmt::Debug for SHA1Hash<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "SHA1Hash({})", self.to_hex_string())
+    }
+}
 
 impl <'a> SHA1Hash<'a> {
     pub fn from_prehashed(raw: &'a [u8]) -> SHA1Hash<'a> {
@@ -22,11 +29,32 @@ impl <'a> SHA1Hash<'a> {
         // TODO: Remove copy here
         SHA1Hash(Cow::Owned(out.to_vec()))
     }
+
+    // from http://illegalargumentexception.blogspot.com/2015/05/rust-byte-array-to-hex-string.html
+    fn to_hex_string(&self) -> String {
+        let &SHA1Hash(ref view) = self;
+        let borrowed: &[u8] = view.borrow();
+        let strs: Vec<String> = borrowed
+            .iter()
+            .map(|b| format!("{:02X}", b))
+            .collect();
+        strs.connect(" ")
+    }
 }
 
 // ASSERT: len(input) % 20 == 0
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 pub struct SHA1Hashes<'a>(pub &'a [u8]);
+
+impl <'a> fmt::Debug for SHA1Hashes<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let strs: Vec<String> = self.iter()
+            .map(|h| format!("{:?}", h))
+            .collect();
+
+        write!(f, "SHA1Hashes[{}]", strs.connect(",\n"))
+    }
+}
 
 impl <'a> SHA1Hashes<'a> {
     pub fn iter(&self) -> iter::Map<Chunks<'a, u8>, fn(&'a [u8]) -> SHA1Hash<'a>> {
